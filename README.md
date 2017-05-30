de9ee8d4707127e0bebe84375f47684119181b11
Luca Radaelli <lradaelli85@users.noreply.github.com>
2017-05-30 10:41:13 +0200

    improved variable naming and messages prompt.Updated README

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 This is a simple script that allows you to create an SSH tunel.
 This is an interactive script,below a little explanation of terms used:
 
--ssh server: ssh server to use for the connection to the remote host
+-ssh server  [ssh proxy]         : ssh server to use for the connection to the remote host
 
 -remote host [destination server]: host that you want to connect to using ssh host
 
-Note: on ssh host you should set AllowTcpForwarding to yes
-If you need to tunnel the connection through the ssh host,you have to open a connection to your localhost ip on the port that you have choosen as local port.
+Note: on ssh server you should set AllowTcpForwarding to yes.
+If you need to tunnel the connection through the ssh server,you have to open a connection to your localhost ip on the port that you have choosen as local port.
 
--- tunnel.sh
+++ tunnel.sh
@@ -3,40 +3,32 @@
 #Author: Luca Radaelli <lradaelli85@users.noreply.github.com>
 
 SSH=`which ssh`
-porta="0"
-porta_locale="0"
-remote_ports="0"
+dst_srv_port="0"
+local_port="0"
+remote_port="0"
 
-while [ -z "$nome" ]; do
-echo "insert the ip address or the hostname of the remote host [destination server]:"
-read nome
+while [ -z "$destination_server" ]; do
+read -p "insert the ip address or the hostname of the destination server: " destination_server
 done
 
-until [[ "$porta" =~ ^-?[0-9]+$ ]] && [ "$porta" -ge 1 ] && [ "$porta" -le 65535 ]; do
-echo "insert the ports of the remote host [destination server] that you want to connect to"
-read porta 
+until [[ "$dst_srv_port" =~ ^-?[0-9]+$ ]] && [ "$dst_srv_port" -ge 1 ] && [ "$dst_srv_port" -le 65535 ]; do
+read -p "insert the ports of the destination server that you want to connect to: " dst_srv_port
 done
 
-until [[ "$porta_locale" =~ ^-?[0-9]+$ ]] && [ "$porta_locale" -ge 1 ] && [ "$porta_locale" -le 65535 ]; do
-echo "insert local port to use"
-echo "maybe you should choose a port between 1025 and 65535"
-read porta_locale 
+until [[ "$local_port" =~ ^-?[0-9]+$ ]] && [ "$local_port" -ge 1 ] && [ "$local_port" -le 65535 ]; do
+read -p "insert local port to use [hint use a port between 1025 and 65535]: " local_port
 done
 
-while [ -z "$utente" ]; do
-echo "insert ssh user to use for the ssh server connection"
-read utente
+while [ -z "$ssh_user" ]; do
+read -p "insert ssh user to use for the ssh proxy connection: " ssh_user
 done
 
-while [ -z "$host_ssh" ]; do
-echo "insert the ip address or the hostname of the ssh server"
-read host_ssh
+while [ -z "$ssh_proxy" ]; do
+read "insert the ip address or the hostname of the ssh proxy: " ssh_proxy
 done
 
-until [[ "$remote_ports" =~ ^-?[0-9]+$ ]] && [ "$remote_ports" -ge 1 ] && [ "$remote_ports" -le 65535 ]; do
-echo "insert the port of the ssh server to use"
-read remote_ports
+until [[ "$remote_port" =~ ^-?[0-9]+$ ]] && [ "$remote_port" -ge 1 ] && [ "$remote_port" -le 65535 ]; do
+read -p "insert the port of the ssh proxy to use for ssh connection: "remote_port
 done
 
-$SSH -N -L $porta_locale:$nome:$porta $utente@$host_ssh -p $remote_ports
-
+$SSH -N -L $local_port:$destination_server:$dst_srv_port $ssh_user@$ssh_proxy -p $remote_port
